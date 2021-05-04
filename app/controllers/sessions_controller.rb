a8acb6ddaaf254b96e10b2d60c66f3df5b0858e7
James Mitchell <jmitchell238@gmail.com>
2021-05-04 14:19:56 -0500

    Lesson 30 Complete - User Authentication

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,8 +5,16 @@ class SessionsController < ApplicationController
   end
 
   def create
-    params[:email]
-    params[:password]
+    user = User.find_by(email: params[:email_or_username]) ||
+           User.find_by(username: params[:email_or_username])
+    # if user && user.authenticate(password: params[:password]) // Same as below
+    if user&.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to user, notice: "Welcome back, #{user.name}!"
+    else
+      flash.now[:alert] = 'Invalid email/password combination!'
+      render :new
+    end
   end
 
   def destroy
