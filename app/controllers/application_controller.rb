46b0085778a9818fcc95d355c53cb4853463f1dd
James Mitchell <jmitchell238@gmail.com>
2021-05-05 14:58:25 -0500

    Lesson 33 Authorization P1 complete

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+
+  private
+
+  def current_user
+    # Store user object in local variable. Prevents multiple database request.
+    # without @current_user ||= you get 3 db request. With it, you get only 1
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  helper_method :current_user
+
+  def require_signin
+    redirect_to new_session_url, alert: 'Please sign in first!' unless current_user
+  end
+
 end
