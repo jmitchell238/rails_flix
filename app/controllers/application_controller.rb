3ce719e7c90b8d1061abfb40080a33273e6570db
James Mitchell <jamesm.mitchell@daveramsey.com>
2021-05-06 15:31:52 -0500

    Lesson 34 Complete

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,9 +16,19 @@ class ApplicationController < ActionController::Base
 
   helper_method :current_user?
 
+  def current_user_admin?
+    current_user&.admin?
+  end
+
+  helper_method :current_user_admin?
+
   def require_signin
     session[:intended_url] = request.url
     redirect_to new_session_url, alert: 'Please sign in first!' unless current_user
+  end
+
+  def require_admin
+    redirect_to movies_url, alert: 'Unauthorized access!' unless current_user_admin?
   end
 
 end
