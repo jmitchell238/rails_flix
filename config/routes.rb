1306b71e2a39d41c78d486880a1274fa2fa686ca
James Mitchell <jamesm.mitchell@daveramsey.com>
2021-05-10 08:02:13 -0500

    Lesson 37 Complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
-  resources :favorites
+
   root 'movies#index'
 
   resources :movies do
     resources :reviews
+    resources :favorites, only: %i[create destroy]
   end
 
-  resource :session, only: [:new, :create, :destroy]
+  resource :session, only: %i[new create destroy]
   get 'signin' => 'sessions#new'
 
   resources :users
